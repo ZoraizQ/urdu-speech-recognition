5e3f2ed6d173dfd4592ca251db722c6923a0af9d
Zoraiz Qureshi <21100130@lums.edu.pk>
2021-04-21 20:16:07 +0000

    add: data prep complete, now train

--- s5/run.sh
+++ s5/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# Copyright 2018 (author: Haris Bin Zia)
-# Apache 2.0
+# 21100130 21100170 21100320 21100297 21100197
 
 . ./cmd.sh
 [ -f path.sh ] && . ./path.sh
@@ -9,17 +7,22 @@ set -e
 
 numLeavesTri1=2000
 numGaussTri1=10000
+
 numLeavesMLLT=2000
 numGaussMLLT=10000
+
 numLeavesSAT=2000
 numGaussSAT=10000
+
 numGaussUBM=400
+
 numLeavesSGMM=7000
 numGaussSGMM=9000
 
-feats_nj=8
-train_nj=8
-decode_nj=8
+# speakers >= no.of jobs
+feats_nj=1
+train_nj=1
+decode_nj=1
 
 echo ============================================================================
 echo "                Data & Lexicon & Language Preparation                     "
@@ -32,7 +35,7 @@ utils/prepare_lang.sh data/local/lang '<oov>' data/local data/lang
 
 utils/format_lm.sh data/lang data/local/lm/LM1.gz data/local/lang/lexicon.txt data/lang_test
 
-#utils/format_lm.sh data/lang data/local/lm/LM2.gz data/local/lang/lexicon.txt data/lang_test
+# utils/format_lm.sh data/lang data/local/lm/LM2.gz data/local/lang/lexicon.txt data/lang_test
 
 echo ============================================================================
 echo "         MFCC Feature Extration & CMVN for Training and Test set          "
@@ -40,10 +43,10 @@ echo ===========================================================================
 
 mfccdir=mfcc
 
-steps/make_mfcc.sh --cmd "$train_cmd" --nj $feats_nj data/train exp/make_mfcc/train $mfccdir
+steps/make_mfcc.sh --cmd $train_cmd --nj $feats_nj data/train exp/make_mfcc/train $mfccdir
 steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train $mfccdir
 
-steps/make_mfcc.sh --cmd "$train_cmd" --nj $feats_nj data/test exp/make_mfcc/test $mfccdir
+steps/make_mfcc.sh --cmd $train_cmd --nj $feats_nj data/test exp/make_mfcc/test $mfccdir
 steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
 
 utils/validate_data_dir.sh data/train
@@ -63,35 +66,41 @@ utils/mkgraph.sh data/lang_test exp/mono exp/mono/graph
 steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
  exp/mono/graph data/test exp/mono/decode
 
+echo ===================================
+echo " FINISHED DATA PREP & MONO TRAIN "
+echo ===================================
+
+
 echo ============================================================================
 echo "           tri1 : Deltas + Delta-Deltas Training & Decoding               "
 echo ============================================================================
 
-steps/align_si.sh --boost-silence 1.25 --nj "$train_nj" --cmd "$train_cmd" \
- data/train data/lang exp/mono exp/mono_ali
+# Align delta-based triphones with boost silence
+steps/align_si.sh --boost-silence 1.25 --nj "$train_nj" --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali
 
-steps/train_deltas.sh --cmd "$train_cmd" \
- $numLeavesTri1 $numGaussTri1 data/train data/lang exp/mono_ali exp/tri1
+# Train delta + delta-delta triphones 
+steps/train_deltas.sh --cmd "$train_cmd" $numLeavesTri1 $numGaussTri1 data/train data/lang exp/mono_ali exp/tri1
 
+# Make language model graph for tri1
 utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph
 
-steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
- exp/tri1/graph data/test exp/tri1/decode
+# Decode graph for tri1
+steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" exp/tri1/graph data/test exp/tri1/decode
 
 echo ============================================================================
 echo "                 tri2 : LDA + MLLT Training & Decoding                    "
 echo ============================================================================
 
-steps/align_si.sh --nj "$train_nj" --cmd "$train_cmd" \
-  data/train data/lang exp/tri1 exp/tri1_ali
+# Align
+steps/align_si.sh --nj "$train_nj" --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_ali
 
-steps/train_lda_mllt.sh --cmd "$train_cmd" \
- $numLeavesMLLT $numGaussMLLT data/train data/lang exp/tri1_ali exp/tri2
+# LDA + MLLT training
+steps/train_lda_mllt.sh --cmd "$train_cmd" $numLeavesMLLT $numGaussMLLT data/train data/lang exp/tri1_ali exp/tri2
 
+# Make and decode
 utils/mkgraph.sh data/lang_test exp/tri2 exp/tri2/graph
 
-steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
- exp/tri2/graph data/test exp/tri2/decode
+steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" exp/tri2/graph data/test exp/tri2/decode
 
 echo ============================================================================
 echo "              tri3 : LDA + MLLT + SAT Training & Decoding                 "
@@ -100,8 +109,7 @@ echo ===========================================================================
 steps/align_si.sh --nj "$train_nj" --cmd "$train_cmd" \
  --use-graphs true data/train data/lang exp/tri2 exp/tri2_ali
 
-steps/train_sat.sh --cmd "$train_cmd" \
- $numLeavesSAT $numGaussSAT data/train data/lang exp/tri2_ali exp/tri3
+steps/train_sat.sh --cmd "$train_cmd" $numLeavesSAT $numGaussSAT data/train data/lang exp/tri2_ali exp/tri3
 
 utils/mkgraph.sh data/lang_test exp/tri3 exp/tri3/graph
 
@@ -109,17 +117,14 @@ steps/decode_fmllr.sh --nj "$decode_nj" --cmd "$decode_cmd" \
  exp/tri3/graph data/test exp/tri3/decode
 
 echo ============================================================================
-echo "                        SGMM2 Training & Decoding                         "
+echo "                      SGMM2 Training & Decoding                           "
 echo ============================================================================
 
-steps/align_fmllr.sh --nj "$train_nj" --cmd "$train_cmd" \
- data/train data/lang exp/tri3 exp/tri3_ali
+steps/align_fmllr.sh --nj "$train_nj" --cmd "$train_cmd" data/train data/lang exp/tri3 exp/tri3_ali
 
-steps/train_ubm.sh --cmd "$train_cmd" \
- $numGaussUBM data/train data/lang exp/tri3_ali exp/ubm4
+steps/train_ubm.sh --cmd "$train_cmd" $numGaussUBM data/train data/lang exp/tri3_ali exp/ubm4
 
-steps/train_sgmm2.sh --cmd "$train_cmd" $numLeavesSGMM $numGaussSGMM \
- data/train data/lang exp/tri3_ali exp/ubm4/final.ubm exp/sgmm2_4
+steps/train_sgmm2.sh --cmd "$train_cmd" $numLeavesSGMM $numGaussSGMM data/train data/lang exp/tri3_ali exp/ubm4/final.ubm exp/sgmm2_4
 
 utils/mkgraph.sh data/lang_test exp/sgmm2_4 exp/sgmm2_4/graph
 
